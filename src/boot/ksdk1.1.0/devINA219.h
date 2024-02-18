8fa3c6ca52a836ee20f0a7f2a1627c152ed4e9a4
Leon Brindley <lpb32@cam.ac.uk>
2024-02-18 20:58:35 +0000

    Updated formatting of devINA219.h.

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -6,10 +6,10 @@ typedef enum
 {
 	kINA219RegConfiguration		= 0x0,
 	kINA219RegShunt		        = 0x1,
-	kINA219RegBus		          = 0x2,
+	kINA219RegBus		        = 0x2,
 	kINA219RegPower		        = 0x3,
-	kINA219RegCurrent		      = 0x4,
-	kINA219RegCalibration		  = 0x5,
+	kINA219RegCurrent		= 0x4,
+	kINA219RegCalibration		= 0x5,
 } INA219Constants;
 
 void		initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts);
