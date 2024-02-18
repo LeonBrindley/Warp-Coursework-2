27697959704206f90352c2bc70f851466c62a524
Leon Brindley <lpb32@cam.ac.uk>
2024-02-18 20:05:17 +0000

    Added register location constants and initINA219() function prototype.

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -1,2 +1,15 @@
 // TI INA219 Datasheet: https://www.vle.cam.ac.uk/mod/resource/view.php?id=9835022
 // Adafruit INA219 User Guide: https://www.vle.cam.ac.uk/mod/resource/view.php?id=9835032
+
+// 0x0: Configuration, 0x1: Shunt, 0x2: Bus, 0x3: Power, 0x4: Current, 0x5: Calibration
+typedef enum
+{
+	kINA219RegConfiguration		= 0x0,
+	kINA219RegShunt		        = 0x1,
+	kINA219RegBus		          = 0x2,
+	kINA219RegPower		        = 0x3,
+	kINA219RegCurrent		      = 0x4,
+	kINA219RegCalibration		  = 0x5,
+} INA219Constants;
+
+void		initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts);
