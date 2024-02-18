66de4b0ea5d99ed4186e91f021bb1bf1d6f5f2b9
Leon Brindley <lpb32@cam.ac.uk>
2024-02-18 20:57:38 +0000

    Added read and write register functions to devINA219.h.

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -13,3 +13,5 @@ typedef enum
 } INA219Constants;
 
 void		initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts);
+WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
+WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister, uint8_t payload);
