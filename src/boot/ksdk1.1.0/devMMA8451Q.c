f693dded9b57320ee112afa0e888a7c94278709a
Leon Brindley <lpb32@cam.ac.uk>
2024-02-29 13:59:18 +0000

    Added new include statements to devMMA8451Q.c.

--- src/boot/ksdk1.1.0/devMMA8451Q.c
+++ src/boot/ksdk1.1.0/devMMA8451Q.c
@@ -56,6 +56,9 @@
 #include "SEGGER_RTT.h"
 #include "warp.h"
 
+#include "devMMA8451Q.h"
+#include "devSSD1331.h"
+#include "activityClassifier.h"
 
 extern volatile WarpI2CDeviceState	deviceMMA8451QState;
 extern volatile uint32_t		gWarpI2cBaudRateKbps;
