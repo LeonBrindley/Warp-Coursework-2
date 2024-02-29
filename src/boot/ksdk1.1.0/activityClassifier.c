a385e6ffd2e001e22fe8a63b669736590f96875f
Leon Brindley <lpb32@cam.ac.uk>
2024-02-29 13:48:09 +0000

    Created activityClassifier.c for Coursework 4.

---
+++ src/boot/ksdk1.1.0/activityClassifier.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/*
+ *	config.h needs to come first
+ */
+#include "config.h"
+
+#include "fsl_misc_utilities.h"
+#include "fsl_device_registers.h"
+#include "fsl_i2c_master_driver.h"
+#include "fsl_spi_master_driver.h"
+#include "fsl_rtc_driver.h"
+#include "fsl_clock_manager.h"
+#include "fsl_power_manager.h"
+#include "fsl_mcglite_hal.h"
+#include "fsl_port_hal.h"
+
+#include "gpio_pins.h"
+#include "SEGGER_RTT.h"
+#include "warp.h"
+
+#include "devMMA8451Q.h"
+#include "devSSD1331.h"
+#include "activityClassifier.h"
