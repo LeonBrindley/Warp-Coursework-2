1b8546799969f9a3bc8cd36c5478128cd817a8d9
Leon Brindley <lpb32@cam.ac.uk>
2024-03-02 15:00:22 +0000

    Fixed number of arguments in configureSensorMMA8451Q().

--- src/boot/ksdk1.1.0/boot.c
+++ src/boot/ksdk1.1.0/boot.c
@@ -3510,7 +3510,9 @@ printAllSensors(bool printHeadersAndCalibration, bool hexModeFlag,
 #if (WARP_BUILD_ENABLE_DEVMMA8451Q)
 	numberOfConfigErrors += configureSensorMMA8451Q(
 		0x00, /* Payload: Disable FIFO */
-		0x01  /* Normal read 8bit, 800Hz, normal, active mode */
+		0x01,  /* Normal read 8bit, 800Hz, normal, active mode */
+		0x20, /* HPF enabled for pulse processing with default HPF cut-off frequency */
+  		0x12 /* Output data high-pass filtered with full-scale range of 8g */
 	);
 #endif
 
