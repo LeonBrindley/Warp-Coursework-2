8f17bfe44c7878fe5fe2acb101a85f4b04f42d4f
Leon Brindley <lpb32@cam.ac.uk>
2024-03-02 13:36:47 +0000

    Planned out functions in activityClassifier.c.

--- src/boot/ksdk1.1.0/activityClassifier.c
+++ src/boot/ksdk1.1.0/activityClassifier.c
@@ -22,3 +22,17 @@
 #include "devMMA8451Q.h"
 #include "devSSD1331.h"
 #include "activityClassifier.h"
+
+// See example implementation at https://www.vle.cam.ac.uk/pluginfile.php/27161189/mod_resource/content/1/chapter-02-measurements-and-uncertainty-and-cover.pdf.
+// As I want to calculate the actual speed, the code below calculates the magnitude of the acceleration instead of simply detecting the maximal axis.
+// Try a sampling rate of 50Hz, which matches the algorithm at https://www.ti.com/lit/pdf/slaa599. Decrease this if there is insufficient memory.
+// The algorithm referenced above requires 1.2 kilobytes of code memory and 640 bytes of data memory. 
+
+// Step 1: Combine all three axes using 3D Pythagoras' theorem.
+
+// Step 2: Low-pass filter the result to smooth out the signal.
+
+// Step 3: Perform extremal value marking by differentiating the data.
+
+// Step 4: Calculate the speed over the last 10 seconds.
+
