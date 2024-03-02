ef1954888e7a47a8fc571bf42092ddad76f7d951
Leon Brindley <lpb32@cam.ac.uk>
2024-03-02 23:42:24 +0000

    Simplified algorithm into only one function in activityClassifier.c.

--- src/boot/ksdk1.1.0/activityClassifier.c
+++ src/boot/ksdk1.1.0/activityClassifier.c
@@ -34,6 +34,52 @@
 
 // Step 1: Combine all three axes using 3D Pythagoras' theorem.
 
+void classifierAlgorithm(){
+  // Set default maximumValue and minimumValue to guarantee that they are updated in the for loop below.
+  maximumValue = 0;
+  minimumValue = 65535;
+	
+  // Store the magnitude of the acceleration in AccelerationBuffer. 
+  AccelerationBuffer[BUFFER_SIZE - 1] =  sqrt((XAcceleration*XAcceleration)
+    + (YAcceleration*YAcceleration) + (ZAcceleration*ZAcceleration));
+  warpPrint("1. Latest Magnitude: %d m/s.\n", AccelerationBuffer[BUFFER_SIZE - 1]);
+  for (int i = 0; i < BUFFER_SIZE - 1; i++){
+    LPFBuffer[i] = AccelerationBuffer[i] * LPFWeights[i];
+    warpPrint("2. LPFBuffer[%d]: %d.\n", i, LPFBuffer[i]);
+    if(LPFBuffer[i] > maximumValue){
+      maximumValue = LPFBuffer[i];
+    }
+    if(LPFBuffer[i] < minimumValue){
+      minimumValue = LPFBuffer[i];
+    } 
+  }
+  LPFBufferMidpoint = (maximumValue - minimumValue) / 2;
+  warpPrint("3. Maximum: %d, Minimum: %d, Midpoint: %d.\n", maximumValue, minimumValue, LPFBufferMidpoint);
+
+  // See https://www.vle.cam.ac.uk/pluginfile.php/27161189/mod_resource/content/1/chapter-02-measurements-and-uncertainty-and-cover.pdf.
+  numberOfCrossings = 0;
+  for(int i = 1; i < BUFFER_SIZE - 1; i++){
+    if(LPFBuffer[i - 1] > LPFBufferMidpoint){
+      if(LPFBuffer[i] < LPFBufferMidpoint){
+        numberOfCrossings++;
+      }
+    }
+    else if(LPFBuffer[i - 1] < LPFBufferMidpoint){
+      if(LPFBuffer[i] > LPFBufferMidpoint){
+        numberOfCrossings++; 
+      }
+    }
+    // Shift AccelerationBuffer and LPFBuffer left to free up space for new data.
+    AccelerationBuffer[i - 1] = AccelerationBuffer[i];
+    LPFBuffer[i - 1] = LPFBuffer[i];
+  }
+  // Average step length between men and women = 0.716m. https://marathonhandbook.com/average-stride-length
+  numberOfSteps += (numberOfCrossings / 2); // Add (numberOfCrossings / 2) to the cumulative numbers of steps since booting the device.
+  Speed = (360 / (1000 * 2))*(numberOfCrossings * 0.716); // 360 10-second periods in an hour. Divide by (1000*2) to convert to km/hr while accounting for both upward and downward crossings.
+  warpPrint("4. Speed: %d.\n", Speed);
+}
+
+/*
 void stepOneCombine(){
   // Shift AccelerationBuffer and LPFBuffer left to free up space for new data.
   for (int i = 1; i < BUFFER_SIZE - 1; i++){
@@ -97,7 +143,7 @@ void stepFourSpeed(){
   Speed = (360 / (1000 * 2))*(numberOfCrossings * 0.716); // 360 10-second periods in an hour. Divide by (1000*2) to convert to km/hr while accounting for both upward and downward crossings.
   warpPrint("4. Speed: %d.\n", Speed);
 }
-
+*/
 WarpStatus updateAccelerations(){
 	uint16_t XLSB, YLSB, ZLSB;
 	uint16_t XMSB, YMSB, ZMSB;
