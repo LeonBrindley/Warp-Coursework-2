7c91046ec0a90cc680e746650e6af2de7674457e
Leon Brindley <lpb32@cam.ac.uk>
2024-03-02 23:43:34 +0000

    Commented out retired functions in devMMA8451Q.c.

--- src/boot/ksdk1.1.0/devMMA8451Q.c
+++ src/boot/ksdk1.1.0/devMMA8451Q.c
@@ -71,11 +71,13 @@ initMMA8451Q(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts)
 		LPFBuffer[i] = 0;
 	}
 
+	/*
 	stepOneCombine();
 	stepTwoFilter();
 	stepThreeMidpoint();
 	stepFourSpeed();
-
+	*/
+	
 	return;
 }
 
