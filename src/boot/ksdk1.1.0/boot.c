d7d800af1f526e3439c0e2d1141ab50af4a14d3f
Leon Brindley <lpb32@cam.ac.uk>
2024-03-04 23:48:43 +0000

    Calling configureSensorMMA8451Q() in active mode instead of standby mode in boot.c.

--- src/boot/ksdk1.1.0/boot.c
+++ src/boot/ksdk1.1.0/boot.c
@@ -3263,7 +3263,7 @@ writeAllSensorsToFlash(int menuDelayBetweenEachRun, int loopForever)
 #if (WARP_BUILD_ENABLE_DEVMMA8451Q)
 	numberOfConfigErrors += configureSensorMMA8451Q(
 		0x00, /* Payload: Disable FIFO */
-		0x00, /* Normal read 14-bit (F_READ = 0), 800Hz, normal, standby mode */
+		0x01, /* Normal read 14-bit (F_READ = 0), 800Hz, normal, active mode */
 		0x00, /* HPF enabled for pulse processing with default HPF cut-off frequency of 16 Hz @ 800Hz ODR */
   		0x12 /* Output data high-pass filtered with full-scale range of 8g */
 	);
@@ -3519,7 +3519,7 @@ printAllSensors(bool printHeadersAndCalibration, bool hexModeFlag,
 #if (WARP_BUILD_ENABLE_DEVMMA8451Q)
 	numberOfConfigErrors += configureSensorMMA8451Q(
 		0x00, /* Payload: Disable FIFO */
-		0x00, /* Normal read 14-bit (F_READ = 0), 800Hz, normal, standby mode */
+		0x01, /* Normal read 14-bit (F_READ = 0), 800Hz, normal, active mode */
 		0x00, /* HPF enabled for pulse processing with default HPF cut-off frequency of 16 Hz @ 800Hz ODR */
   		0x12 /* Output data high-pass filtered with full-scale range of 8g */
 	);
