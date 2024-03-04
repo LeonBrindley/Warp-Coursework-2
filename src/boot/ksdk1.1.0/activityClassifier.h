29cb90828f6ca409d39b8e0bc07d240d64f9d096
Leon Brindley <lpb32@cam.ac.uk>
2024-03-04 00:51:12 +0000

    Initialised acceleration measurements to 0 in activityClassifier.h.

--- src/boot/ksdk1.1.0/activityClassifier.h
+++ src/boot/ksdk1.1.0/activityClassifier.h
@@ -29,8 +29,10 @@ WarpStatus updateAccelerations();
 #define SAMPLE_PERIOD 0.01 // Take a sample every 10ms.
 #define BUFFER_SIZE 19 // Size of AccelerationBuffer and LPFBuffer. LPF must have an odd number of taps.
 
-// Define variables for the X, Y and Z acceleration measurements.
-int16_t XAcceleration, YAcceleration, ZAcceleration;
+// Define variables for the X, Y and Z acceleration measurements. Initialise to 0.
+int16_t XAcceleration = 0;
+int16_t YAcceleration = 0;
+int16_t ZAcceleration = 0;
 
 // Define buffers to store the magnitudes of the acceleration measurements before and after low-pass filtering.
 uint16_t AccelerationBuffer[BUFFER_SIZE] = {0}; // Initialised to 0.
@@ -44,7 +46,7 @@ float speed = 0; // Estimated speed in km/hr. Initialised to 0.
 
 // The LPF uses a finite impulse response (FIR) structure.
 // The FIR coefficients h(n) are defined below.
-// These have been calculated using https://rfcalculator.com/FIR-Filters.
+// These have been calculated using https://rfcalculator.com/FIR-Filters and then multiplied by 10,000.
 int16_t LPFWeights[BUFFER_SIZE] = {0, 3, 13, 43, 102, 198, 323, 451, 549, 586, 549, 451, 323, 198, 102, 43, 13, 3, 0};
 
 void printGUI();
