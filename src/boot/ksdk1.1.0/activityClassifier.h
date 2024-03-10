72792da467c9233fb2e955b1be10813545efd3d4
Leon Brindley <lpb32@cam.ac.uk>
2024-03-10 17:01:19 +0000

    Renamed cumulativeNumberOfInflectionPoints to cumulativeInflectionPoints in activityClassifier.h.

--- src/boot/ksdk1.1.0/activityClassifier.h
+++ src/boot/ksdk1.1.0/activityClassifier.h
@@ -50,7 +50,7 @@ uint32_t maximumValue, minimumValue; // Not set until the classification algorit
 uint32_t LPFBufferMidpoint; // Not set until the classification algorithm runs.
 uint16_t numberOfCrossings = 0; // The number of times the midpoint of the low-pass filtered signal is crossed.
 uint16_t numberOfInflectionPoints = 0; // The number of local maxima and minima in the array. Use numberOfInflectionPoints / 2 instead of the number of steps to avoid rounding errors.
-uint16_t cumulativeNumberOfInflectionPoints = 0; // The cumulative number of inflection points since starting the program.
+uint16_t cumulativeInflectionPoints = 0; // The cumulative number of inflection points since starting the program.
 uint16_t cumulativeNumberOfSteps = 0; // Cumulative number of steps since booting the device. Initialised to 0.
 
 uint32_t distance = 0; // Initialised to 0.
