bd56df77d50efbbca437751bdaaed6ea98a8c81d
Leon Brindley <lpb32@cam.ac.uk>
2024-03-10 17:05:14 +0000

    Removed old variables from activityClassifier.h.

--- src/boot/ksdk1.1.0/activityClassifier.h
+++ src/boot/ksdk1.1.0/activityClassifier.h
@@ -38,7 +38,7 @@ int16_t ZCombined = 0;
 uint32_t accelerationMagnitude = 0;
 // uint32_t exampleData[BUFFER_SIZE] = {500, 739, 920, 998, 954, 799, 570, 324, 121, 11, 20, 147, 360, 607, 828, 968, 994, 899, 706}; // Sine wave with known frequency of 0.5Hz for testing.
 // uint8_t exampleDataCounter = 0; // For selecting the correct array elements in exampleData in sequence.
-uint8_t cycleCounter = 0; // This ensures the cumulative number of steps is only added to when the buffer has been fully refreshed to avoid double-counting.
+uint8_t cycleCounter = 0; // This ensures that cumulativeInflectionPoints is only added to when the buffer has been fully refreshed (i.e. cycleCounter = BUFFER_SIZE) to avoid double-counting.
 
 // Declare activityReading as the enumerated type detectedActivity defined above.
 detectedActivity activityReading;
@@ -46,12 +46,9 @@ detectedActivity activityReading;
 // Define buffers to store the magnitudes of the acceleration measurements before and after low-pass filtering.
 uint32_t AccelerationBuffer[BUFFER_SIZE] = {0}; // Initialised to 0.
 uint32_t LPFBuffer[BUFFER_SIZE] = {0}; // Initialised to 0.
-uint32_t maximumValue, minimumValue; // Not set until the classification algorithm runs.
-uint32_t LPFBufferMidpoint; // Not set until the classification algorithm runs.
-uint16_t numberOfCrossings = 0; // The number of times the midpoint of the low-pass filtered signal is crossed.
 uint16_t numberOfInflectionPoints = 0; // The number of local maxima and minima in the array. Use numberOfInflectionPoints / 2 instead of the number of steps to avoid rounding errors.
 uint16_t cumulativeInflectionPoints = 0; // The cumulative number of inflection points since starting the program.
-uint16_t cumulativeNumberOfSteps = 0; // Cumulative number of steps since booting the device. Initialised to 0.
+uint16_t cumulativeSteps = 0; // Cumulative number of steps since booting the device. Initialised to 0.
 
 uint32_t distance = 0; // Initialised to 0.
 uint32_t speed = 0; // Initialised to 0.
@@ -63,7 +60,7 @@ uint32_t speed = 0; // Initialised to 0.
 // uint16_t LPFWeights[BUFFER_SIZE] = {0, 3, 40, 238, 875, 2303, 4664, 7555, 10014, 10986, 10014, 7555, 4664, 2303, 875, 238, 40, 3, 0}; // NEW 19-element array.
 uint16_t LPFWeights[BUFFER_SIZE] = {0, 0, 4, 21, 82, 243, 604, 1316, 2578, 4618, 7658, 11853, 17232, 23649, 30751, 37991, 44691, 50136, 53694, 54932, 53694, 50136, 44691, 37991, 30751, 23649, 17232, 11853, 7658, 4618, 2578, 1316, 604, 243, 82, 21, 4, 0, 0}; // 39-element array.
 
-uint32_t sqrtInt(uint32_t base);
+uint32_t sqrtInt(uint32_t base); // Return the square root of an integer using the Newton-Raphson method.
 void simpleDiff(); // Function to identify maxima and minima by considering the gradient on either side of each data point.
 
 // void printGUI();
