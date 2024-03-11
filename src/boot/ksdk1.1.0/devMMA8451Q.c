1fc732b0eae0147055b9323be858c3c25adf19ec
Leon Brindley <lpb32@cam.ac.uk>
2024-03-11 01:25:45 +0000

    Changed full-scale range to +/-8g in devMMA8451Q.c.

--- src/boot/ksdk1.1.0/devMMA8451Q.c
+++ src/boot/ksdk1.1.0/devMMA8451Q.c
@@ -72,7 +72,7 @@ initMMA8451Q(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts)
 		0x00, /* Payload: Disable FIFO */
 		0x05, /* Normal read 14-bit (F_READ = 0), 800Hz, reduced noise, active mode */
 		0x00, /* HPF enabled for pulse processing with default HPF cut-off frequency of 16 Hz @ 800Hz ODR */
-  		0x11 /* Output data high-pass filtered with full-scale range of 4g */
+  		0x12 /* Output data high-pass filtered with full-scale range of +/-8g */
 	);
 	warpPrint("configErrors: %d.\n", configErrors);
 	
